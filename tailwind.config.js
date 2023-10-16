d5390d8c83c000a425e7a48be4d875acb2ba47ca
Dhirajrai2221 <113769910+Dhirajrai2221@users.noreply.github.com>
2023-10-16 19:15:46 +0545

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors:{
+        dark_green: '#3A5B22',
+        bright_blue: '#0F3DDE',
+        line_bg: '#F5F5F5',
+        input_color: '#d9d9d9'
+      }
+    },
   },
   plugins: [],
-}
-
+};
